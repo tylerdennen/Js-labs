c0aaa2831361635d6e17373d7236a6ab0045e90a
Yulia_Tarasenko <tyler000dennen@gmail.com>
2019-09-22 02:42:44 +0300

    Add another solution to 4-count-types.js

--- Lab2(Data Types)/4-count-types.js
+++ Lab2(Data Types)/4-count-types.js
@@ -3,7 +3,8 @@
 const countTypesInArray = array => {
   const result = {};
   for (const obj of array) {
-    result[typeof obj] = (result[typeof obj] ? result[typeof obj] + 1 : 1);
+    result[typeof obj] = result[typeof obj] + 1 || 1;
+    // result[typeof obj] = (result[typeof obj] ? result[typeof obj] + 1 : 1);
   }
   return result;
 };
